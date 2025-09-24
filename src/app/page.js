db15a52af77aa2491e395e4676e1bcfb0adb9dc7
lanutritia-eng <lanutritia@gmail.com>
2025-09-24 11:18:57 -0600

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-
+import Reveal from "@/components/Reveal";
 import { motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
